6ce9dfdfea8c4f5d90c8c531efb1b58240da44ac
Eric Lewis <ericlewis777@gmail.com>
2014-05-03 16:04:01 -0700

    fix autolayout problem add api client add location pod

---
+++ weedly/models/EELWMClient.h
@@ -0,0 +1,13 @@
+//
+//  EELWMClient.h
+//  weedly
+//
+//  Created by 1debit on 5/3/14.
+//  Copyright (c) 2014 Eric Lewis. All rights reserved.
+//
+
+#import "OVCClient.h"
+
+@interface EELWMClient : OVCClient
+
+@end
---
+++ weedly/models/EELWMClient.m
@@ -0,0 +1,13 @@
+//
+//  EELWMClient.m
+//  weedly
+//
+//  Created by 1debit on 5/3/14.
+//  Copyright (c) 2014 Eric Lewis. All rights reserved.
+//
+
+#import "EELWMClient.h"
+
+@implementation EELWMClient
+
+@end
