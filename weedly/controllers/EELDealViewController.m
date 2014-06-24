c416306ebaae84aa7ae068a986893a3f1135760a
Eric Lewis <ericlewis777@gmail.com>
2014-06-24 15:30:01 -0700

    close #26

--- weedly/controllers/EELDealViewController.m
+++ weedly/controllers/EELDealViewController.m
@@ -35,6 +35,16 @@
 
 - (void)webViewDidFinishLoad:(UIWebView *)webView{
     [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+    
+    NSString* css = @"\"body { background-color: #FFFFFF;} .wm-listing-bordered-section { min-height: 0; margin: 0 0 15px 0; } .wm-listing-bordered-section-header { background-color: #fafafa; color: black; } .deal-empty, .deal-empty span { background-color: #FFFFFF; color: #000000; } \"";
+    NSString* js = [NSString stringWithFormat:
+                    @"var styleNode = document.createElement('style');\n"
+                    "styleNode.type = \"text/css\";\n"
+                    "var styleText = document.createTextNode(%@);\n"
+                    "styleNode.appendChild(styleText);\n"
+                    "document.getElementsByTagName('head')[0].appendChild(styleNode);\n"
+                    "document.getElementsByTagName('img')[0].remove();\n",css];
+    [self.webView stringByEvaluatingJavaScriptFromString:js];
 }
 
 - (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error{
