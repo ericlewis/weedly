95e1c7bf87170369120c7c4c246dde5a9882489e
Eric Lewis <ericlewis777@gmail.com>
2014-07-06 12:58:57 -0700

    no need

--- weedly/EELAppDelegate.m
+++ weedly/EELAppDelegate.m
@@ -69,10 +69,6 @@
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    
-    NSUserDefaults * standardUserDefaults = [NSUserDefaults standardUserDefaults];
-    BOOL alarm_preference = [[standardUserDefaults objectForKey:@"420_alarm_preference"] boolValue];
-    [self toggleAlarm:alarm_preference];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
