91151fc415b50de4ca31cc457522fd627f4896ce
Eric Lewis <ericlewis777@gmail.com>
2014-07-06 12:53:49 -0700

    working 420 alarm

--- weedly/EELAppDelegate.m
+++ weedly/EELAppDelegate.m
@@ -24,7 +24,38 @@
     UINavigationController *front = [[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateViewControllerWithIdentifier:@"FrontViewController"];
     self.window.rootViewController = front;
     
+    NSUserDefaults *standardUserDefaults = [NSUserDefaults standardUserDefaults];
+    NSString *alarm_preference = [standardUserDefaults objectForKey:@"420_alarm_preference"];
+    if (!alarm_preference) {
+        [self registerDefaultsFromSettingsBundle];
+    }
+    
     return YES;
+}
+
+- (void)registerDefaultsFromSettingsBundle {
+    // this function writes default settings as settings
+    NSString *settingsBundle = [[NSBundle mainBundle] pathForResource:@"Settings" ofType:@"bundle"];
+    if(!settingsBundle) {
+        NSLog(@"Could not find Settings.bundle");
+        return;
+    }
+    
+    NSDictionary *settings = [NSDictionary dictionaryWithContentsOfFile:[settingsBundle stringByAppendingPathComponent:@"Root.plist"]];
+    NSArray *preferences = [settings objectForKey:@"PreferenceSpecifiers"];
+    
+    NSMutableDictionary *defaultsToRegister = [[NSMutableDictionary alloc] initWithCapacity:[preferences count]];
+    
+    for(NSDictionary *prefSpecification in preferences) {
+        NSString *key = [prefSpecification objectForKey:@"Key"];
+        if(key) {
+            [defaultsToRegister setObject:[prefSpecification objectForKey:@"DefaultValue"] forKey:key];
+            NSLog(@"writing as default %@ to the key %@",[prefSpecification objectForKey:@"DefaultValue"],key);
+        }
+    }
+    
+    [[NSUserDefaults standardUserDefaults] registerDefaults:defaultsToRegister];
+    
 }
 							
 - (void)applicationWillResignActive:(UIApplication *)application
@@ -37,6 +68,10 @@
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    NSUserDefaults * standardUserDefaults = [NSUserDefaults standardUserDefaults];
+    BOOL alarm_preference = [[standardUserDefaults objectForKey:@"420_alarm_preference"] boolValue];
+    [self toggleAlarm:alarm_preference];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
@@ -47,11 +82,23 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
+    NSUserDefaults *standardUserDefaults = [NSUserDefaults standardUserDefaults];
+    BOOL alarm_preference = [[standardUserDefaults objectForKey:@"420_alarm_preference"] boolValue];
+    [self toggleAlarm:alarm_preference];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+- (void)toggleAlarm:(BOOL)switchEnabled {
+    if (switchEnabled) {
+        [self scheduleAlarm];
+    }else{
+        [self cancelAlarm];
+    }
 }
 
 - (void)scheduleAlarm{
